28b7a45609b7e76f40929f3f5949bf067e48fc8c
ahmad <ahmadshaher20061@gmail.com>
2026-05-20 16:18:03 +0300

    Fix Express v5 wildcard route

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use('/api/movies', movieRoutes);
 
 // Serve React frontend
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
